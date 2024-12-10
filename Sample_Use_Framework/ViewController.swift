65eda52370b0a1f085d9928313b04a5820453cba
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-10 23:55:18 +0900

    Initial Commit

---
+++ Sample_Use_Framework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample_Use_Framework
+//
+//  Created by ウルトラ深瀬 on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
